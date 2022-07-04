7d23bc1cd449d9cc5db7be475c818b1a4f0389f5
PhamVanDat2000 <phamvandat.hcmut@gmail.com>
2022-07-04 17:32:21 +0700

    add Shop page

--- resources/js/pages/shop/Shop.jsx
+++ resources/js/pages/shop/Shop.jsx
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import CardItem from '../../components/cardItem/CardItem';
+import Footer from '../../components/footer/Footer';
+import Pagination from 'react-bootstrap/Pagination';
 
 export default function Shop() {
 	const book = [{
@@ -76,16 +78,68 @@ export default function Shop() {
 		discount_price: 11000,
 	}
 	]
+	const categoryList = ['trinh tham', 'phieu luu', 'lang man', 'truyen tranh']
 	return (
 		<div>
 			<NavBar />
-			<Container>
+			<Container className='shop-container'>
 				<Row>
-					<Col sm={2} md={2} lg={2}>
-						filter by
+					<Col sm={4} md={4} lg={2}>
+						<h6>Filter by</h6>
+
+						<div className="block-filter">
+							<h1>Category</h1>
+							<ul>
+								{categoryList.map((ele, idx) => {
+									return (
+										<li key={idx}>{ele}</li>
+									)
+								})}
+							</ul>
+						</div>
+						<div className="block-filter">
+							<h1>Author</h1>
+							<ul>
+								{categoryList.map((ele, idx) => {
+									return (
+										<li key={idx}>{ele}</li>
+									)
+								})}
+							</ul>
+						</div>
+						<div className="block-filter">
+							<h1>Rating Review</h1>
+							<ul>
+								{[1, 2, 3, 4, 5].map((ele, idx) => {
+									return (
+										<li key={idx}>{ele} Star</li>
+									)
+								})}
+							</ul>
+						</div>
 					</Col>
 
-					<Col sm={10} md={12} lg={10}>
+					<Col sm={8} md={8} lg={10}>
+						<div className="sorting-container d-flex justify-content-between">
+							<h6>Showing 1-20 of 126 books</h6>
+							<div className="d-flex justify-content-end">
+								<select class="form-select sort-select" aria-label="Default select example">
+									<option value="0" selected>Sort by on sale</option>
+									<option value="1">Sort by popular</option>
+									<option value="2">Sort by price low</option>
+									<option value="3">Sort by price high</option>
+								</select>
+
+								<select class="form-select filter-number-select" aria-label="Default select example">
+									<option value="0" selected>Show 20</option>
+									<option value="1">Show 15</option>
+									<option value="2">Show 10</option>
+									<option value="3">Show 5</option>
+								</select>
+							</div>
+						</div>
+
+
 						<Row xs={1} md={2} lg={4} className='g-4'>
 							{book.map((ele, idx) => {
 								return (
@@ -95,9 +149,19 @@ export default function Shop() {
 								)
 							})}
 						</Row>
+						<nav aria-label="Page navigation example" className='d-flex justify-content-center'>
+							<ul class="pagination">
+								<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+								<li class="page-item"><a class="page-link" href="#">1</a></li>
+								<li class="page-item"><a class="page-link" href="#">2</a></li>
+								<li class="page-item"><a class="page-link" href="#">3</a></li>
+								<li class="page-item"><a class="page-link" href="#">Next</a></li>
+							</ul>
+						</nav>
 					</Col>
 				</Row>
 			</Container>
+			<Footer />
 		</div>
 	)
 }
