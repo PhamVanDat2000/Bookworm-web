ea45fac6bbbb4716c9b82861b31f26bd10cc6962
PhamVanDat2000 <phamvandat.hcmut@gmail.com>
2022-07-08 22:58:44 +0700

    add shopRequest file

---
+++ app/Http/Requests/ShopRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ShopRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'filter' => [
+                'required',
+                Rule::in(['sale', 'popular', 'price'])
+            ],
+            'per_page'=>'required|numeric',
+            'order'=>[
+                Rule::in(['desc', 'asc'])
+    ],
+        ];
+    }
+}
