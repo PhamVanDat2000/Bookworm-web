03d550de70fbbe86bd76bbbe37c7308c657f96ab
PhamVanDat2000 <phamvandat.hcmut@gmail.com>
2022-07-08 23:09:35 +0700

    switch to using redux instead of useState

--- resources/js/components/addToCard/AddToCard.jsx
+++ resources/js/components/addToCard/AddToCard.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card';
-export default function AddToCard() {
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../features/card/cardSlice';
+import ButtonCustom from '../button/ButtonCustom';
+
+
+export default function AddToCard(props) {
+	const { book } = props
 	const [quantity, setQuantity] = useState(0)
+	const dispatch = useDispatch()
+
+	const handleMinus = () => {
+		if (quantity > 0) {
+			setQuantity(quantity - 1)
+			dispatch(decrement())
+		}
+	}
+	const handleAdd = () => {
+		setQuantity(quantity + 1)
+		dispatch(increment())
+	}
 	return (
 		<Card className='add-to-card'>
 			<Card.Header className="d-flex justify-content-start">
-				<span className="book-price">10000</span>
-				<span className="discount-price">9000</span>
+				<span className="book-price">{book.book_price}</span>
+				<span className="discount-price">{book.book_disscount}</span>
 			</Card.Header>
 			<Card.Body className="d-flex flex-column align-items-center">
 				<h6>Quantity</h6>
 				<div className="quatity-card">
-					<span onClick={() => { if (quantity > 0) setQuantity(quantity - 1) }}>-</span>
+					<span onClick={() => handleMinus()}>-</span>
 					<span>{quantity}</span>
-					<span onClick={() => setQuantity(quantity + 1)}>+</span>
+					<span onClick={() => handleAdd()}>+</span>
 				</div>
-				<button className='btn-add-to-card'>Add to card</button>
+				<div className='d-flex justify-content-center w-100'>
+					<ButtonCustom text={'Add to card'} />
+				</div>
 			</Card.Body>
 		</Card>
 	)
