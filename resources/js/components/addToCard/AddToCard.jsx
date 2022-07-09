888a75bb16ddc3a3adeecb2a16a2e2ff6c33d651
PhamVanDat2000 <phamvandat.hcmut@gmail.com>
2022-07-10 01:44:10 +0700

    fix change price when quantity change

--- resources/js/components/addToCard/AddToCard.jsx
+++ resources/js/components/addToCard/AddToCard.jsx
@@ -7,31 +7,33 @@ import ButtonCustom from '../button/ButtonCustom';
 
 export default function AddToCard(props) {
 	const { book } = props
-	const [quantity, setQuantity] = useState(0)
+	const [quantity, setQuantity] = useState(1)
 	const dispatch = useDispatch()
 
 	const handleMinus = () => {
-		if (quantity > 0) {
+		if (quantity > 1) {
 			setQuantity(quantity - 1)
 			dispatch(decrement())
 		}
 	}
 	const handleAdd = () => {
-		setQuantity(quantity + 1)
-		dispatch(increment())
+		if (quantity < 8) {
+			setQuantity(quantity + 1)
+			dispatch(increment())
+		}
 	}
 	return (
 		<Card className='add-to-card'>
 			<Card.Header className="d-flex justify-content-start">
-				<span className="book-price">{book.book_price}</span>
-				<span className="discount-price">{book.book_disscount}</span>
+				<span className="book-price">${(book.book_price * quantity).toFixed(2)}</span>
+				<span className="discount-price">${(book.discount_price * quantity).toFixed(2)}</span>
 			</Card.Header>
 			<Card.Body className="d-flex flex-column align-items-center">
 				<h6>Quantity</h6>
 				<div className="quatity-card">
-					<span onClick={() => handleMinus()}>-</span>
+					<button onClick={() => handleMinus()}>-</button>
 					<span>{quantity}</span>
-					<span onClick={() => handleAdd()}>+</span>
+					<button onClick={() => handleAdd()}>+</button>
 				</div>
 				<div className='d-flex justify-content-center w-100'>
 					<ButtonCustom text={'Add to card'} />
